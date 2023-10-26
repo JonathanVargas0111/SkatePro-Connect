a44af69ea3468d8ce5ba5a4c0bbc6bba35572038
JonathanVargas0111 <jonathanfelipe0111@hotmail.com>
2023-10-26 12:30:57 -0500

    adding fonts search later

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import FeatureSection from "@/components/FeatureSection/FeatureSection";
 import Hero from "@/components/Hero";
 
+import { Roboto } from 'next/font/google'
+const roboto = Roboto({ weight: ['100', '300', '400', '500', '700', '900'], subsets: ['latin'] });
 
 
 export default function Home() {
+
   const backgroundStyle = {
     backgroundImage: `url(${'imgBackground.png'})`,
   };
 
   return (
-    <main className="bg-center bg-repeat" /* style={backgroundStyle} */>
+    <main className={`bg-center bg-repeat ${roboto.className}`} /* style={backgroundStyle} */>
       <Hero />
       <FeatureSection />
     </main>
